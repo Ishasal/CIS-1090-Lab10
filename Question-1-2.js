ef7cdee9ee15f53d00a167d18a8a4571bff5d2c6
IshaSal <you@example.com>
2022-11-07 10:56:31 -0500

    Lab 10 changes

--- Question-1-2.js
+++ Question-1-2.js
@@ -1,4 +1,8 @@
 //❓❓ LAB QUESTION 1, 2:
-let student = {};
+let student = {
+    firstName: "Isha",
+    lastName: "Sal",
+    classes: [ "Javascript", "English Comp", "Statistics 1", "Digital Photography", "Pricipals of Management"]
+};
 
 console.log(student);
